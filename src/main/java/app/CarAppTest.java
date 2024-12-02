fecdf6f803786fa3e0bab931bafabce90732d57d
rohitlavate97 <rohitlavate97@gmail.com>
2024-12-02 10:26:59 +0530

    changes in scope

--- src/main/java/app/CarAppTest.java
+++ src/main/java/app/CarAppTest.java
@@ -8,7 +8,12 @@ public class CarAppTest {
     public static void main(String[] args) {
 
         ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
-        CarService carService=context.getBean(CarService.class);
-        carService.drive();
+//        CarService carService=context.getBean(CarService.class);
+//        carService.drive();
+        CarService c1=context.getBean(CarService.class);
+        System.out.println(c1.hashCode());
+        CarService c2=context.getBean(CarService.class);
+        System.out.println(c2.hashCode());
+        //the commented code is to show that only one object is created by IOC
     }
 }
